8b433fb01adfe16f3d2304e62a2c89ed228f0642
wangzy <wangzy@sw2.com>
2022-08-15 16:03:20 +0800

    新增webpack配置文件，项目初始配置结构模板完成

---
+++ code/chapter01/part3/src/index.ts
@@ -0,0 +1,19 @@
+import {hi} from "./m1";
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+const obj = {name: 'Alex', age: 33}
+
+console.log(obj);
+
+obj.age = 18;
+
+console.log(obj)
+
+console.log(sum(123, 456));
+console.log("eiheihei");
+console.log(hi);
+
+console.log(Promise)
---
+++ code/chapter01/part3/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+// webpack中的所有配置信息都应该写在module.exports中
+module.exports = {
+    mode: "development",
+    // 指定入口文件
+    entry: "./src/index.ts",
+
+    // 指定打包文件所在目录
+    output: {
+        // 指定打包文件的目录
+        path: path.resolve(__dirname, 'dist'),
+        // 打包后文件的名字
+        filename: "bundle.js",
+
+        // 告诉webpack不使用箭头函数
+        // environment: {
+        //     arrowFunction: false
+        // }
+    },
+
+    // 指定webpack打包时要使用的模块
+    module: {
+        // 指定要加载的规则
+        rules: [
+            {
+                // test指定规则生效的文件类型
+                test: /\.ts$/,
+                // 要使用的loader
+                use: [
+                    // 配置babel
+                    {
+                        // 指定加载器
+                        loader: "babel-loader",
+                        // 设置babel
+                        options: {
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        // 要兼容的目标浏览器
+                                        targets: {
+                                            "chrome": "58",
+                                            "ie": "8"
+                                        },
+
+                                        // 指定corejs的版本
+                                        "corejs": "3",
+                                        // 使用corejs的方式"usage" 表示按需加载
+                                        "useBuiltIns": 'usage'
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                    , 'ts-loader'],
+                // 要排除的文件
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    // 配置webpack插件
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            // title: "欸嘿"
+            template: "./src/index.html"
+        }),
+
+    ],
+
+    // 设置引用模块
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
