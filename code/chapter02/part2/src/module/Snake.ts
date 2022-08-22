95709e978f54309e9ecbf4c0221d0bac8bbedf6f
wangzy <utopianpuppy@qq.com>
2022-08-22 22:25:27 +0800

    游戏完成

--- code/chapter02/part2/src/module/Snake.ts
+++ code/chapter02/part2/src/module/Snake.ts
@@ -1,6 +1,6 @@
 class Snake {
     head: HTMLElement;
-    bodies: HTMLCollection;
+    bodies: HTMLCollectionOf<HTMLElement>;
     element: HTMLElement;
 
     constructor() {
@@ -26,7 +26,21 @@ class Snake {
             throw new Error('GAME OVER!')
         }
 
+        // 修改X时，是在修改水平坐标，蛇在左右移动，不能向反方向掉头
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            // 如果发生了掉头，则继续往当前方向移动
+            if (value > this.X) {
+                value = this.X - 10
+            } else {
+                value = this.X + 10
+            }
+        }
+
+        this.moveBody();
+
         this.head.style.left = value + 'px';
+
+        this.checkCrash()
     }
 
     set Y(value) {
@@ -38,11 +52,43 @@ class Snake {
             throw new Error('GAME OVER!')
         }
 
+        // 修改Y时，是在修改垂直坐标，蛇在上下移动，不能向反方向掉头
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            // 如果发生了掉头，则继续往当前方向移动
+            if (value > this.Y) {
+                value = this.Y - 10
+            } else {
+                value = this.Y + 10
+            }
+        }
+
+        this.moveBody();
+
         this.head.style.top = value + 'px';
+
+        this.checkCrash()
     }
 
     addBody() {
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
+    }
+
+    moveBody() {
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+
+            (this.bodies[i] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+        }
+    }
+
+    checkCrash() {
+        for (let i = 1; i < this.bodies.length; i++) {
+            if (this.X === this.bodies[i].offsetLeft && this.Y === this.bodies[i].offsetTop) {
+                throw new Error('检测到碰撞！')
+            }
+        }
     }
 }
 
